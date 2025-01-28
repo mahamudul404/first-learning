393c2be537df479f20294c292420aadd71d06a94
Your New Name <mahamudul408432@gmail.com>
2025-01-28 22:01:36 +0600

    implement data retrieval function using XMLHttpRequest

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const getButton = document.getElementById("getDataBtn");
 const sendButton = document.getElementById("sendDataBtn");
 
 const getData = function () {
+  const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
+    xhr.send();
 
+    xhr.onload = function () {
+      const result = xhr.responseText;
+      console.log(result);
+    }
 }
 
 const sendData = function () {
