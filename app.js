49457f3e04dcdc8698bf79dcbcf49a56b3d25931
Your New Name <mahamudul408432@gmail.com>
2025-01-28 21:52:38 +0600

    implement data retrieval and sending functions with event listeners

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const getButton = document.getElementById("getDataBtn");
 const sendButton = document.getElementById("sendDataBtn");
+
+const getData = function () {
+
+}
+
+const sendData = function () {
+  
+}
+
+getButton.addEventListener("click", getData);
+sendButton.addEventListener("click", sendData);
